a5cebe96e4f5f7df663d8c9272cfe7d996c71fb8
morysol <cepera.cMupHoB@gmail.com>
2022-04-26 09:23:00 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,15 +1,6 @@
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-aio-3.2.5.min.js';
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
-
 const refs = {
   form: document.querySelector('.form'),
   formInput: document.querySelector('.form[name="delay"]'),
@@ -27,25 +18,42 @@ function formOnSubmit(event) {
     elements: { delay, step, amount },
   } = event.target;
 
-  console.log(delay.value, step.value, amount.value);
+  if (amount.value < 1) {
+    event.target.reset();
+    console.log('The amount must be >= 1');
+    return;
+  }
 
-  // const {
-  //   elements: { email, message },
-  // } = event.target;
+  for (
+    let i = 1, myDelay = Number.parseInt(delay.value);
+    i <= amount.value;
+    i += 1, myDelay += Number.parseInt(step.value)
+  ) {
+    console.log(myDelay);
 
-  // if (email.value === '' || message.value === '') {
-  //   return console.log('Please fill in all the fields!');
-  // }
+    createPromise(i, myDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay} ms`);
+        console.log(`✅ Fulfilled promise ${position} in ${delay} ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay} ms`);
+        console.log(`❌ Rejected promise ${position} in ${delay} ms`);
+      });
+  }
 
-  // console.log(` ------> submited email: "${email.value}", message: "${message.value}"`);
-
-  // localStorage.removeItem(localStorageLable);
+  function createPromise(position, delay) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  }
 
   event.target.reset();
 }
-
-// refs.formButton.addEventListener('click', formOnSubmit);
-// function formOnSubmit() {
-//   console.log('submitted');
-//   console.log(refs);
-// }
